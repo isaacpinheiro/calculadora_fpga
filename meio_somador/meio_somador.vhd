5b57cd5b9a9f9ca73dbaf012fac1e1014b50595f
Isaac Pinheiro <isaacpnhr@gmail.com>
2018-08-15 18:16:39 -0300

    Implementando um meio somador.

---
+++ meio_somador/meio_somador.vhd
@@ -0,0 +1,16 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+
+entity meio_somador is
+	port(
+		a, b : in std_logic;
+		s, cout : out std_logic
+	);
+end meio_somador;
+
+architecture logic of meio_somador is
+begin
+	s <= a xor b;
+	cout <= a and b;
+end logic;
